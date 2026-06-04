8b2c1006ae1219efc8f8986520edb35cfa96b49f
WFeree <ivacyka12@gmail.com>
2026-06-04 16:20:31 +0200

    Layout for game

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,39 +1,55 @@
 "use client"
 
+import Health from "@/components/Health"
 import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
-import { Heart } from "lucide-react"
-import { useState } from "react"
+import { Card, CardAction, CardContent, CardTitle } from "@/components/ui/card"
 
 const page = () => {
-  const [health, setHealth] = useState<number>(20)
+
   return (
     <div className="flex pt-10 items-center min-h-screen flex-col">
       <h1 className="text-4xl font-bold mb-10">SCOUNDREL</h1>
       <div className="grid grid-cols-5 grid-rows-2 gap-4 w-full min-h-200 p-4">
-        <div className="border">
-          <h1>dungeon</h1>
-        </div>
-        <div className="border col-span-3">
-          <h1>room</h1>
-        </div>
-        <div className="border">
-          <h1>throwaway</h1>
-        </div>
         <div className="flex flex-col justify-center items-center">
           <Card>
-            <CardContent className="flex gap-4 items-center">
-              <Heart size={24} strokeWidth={4} color={"red"}/>
-              <span className="text-2xl">{health}</span>
+            <CardTitle className="text-center">Dungeon</CardTitle>
+            <CardContent className="flex gap-4 items-center relative">
+              <div className="w-30 h-48 bg-black/10 rounded border border-gray-900"></div>
             </CardContent>
           </Card>
         </div>
-        <div className="border col-span-3">
-          <h1>equipped</h1>
+        <div className="w-full flex col-span-3 flex-col justify-center items-center">
+          <Card>
+            <CardTitle className="text-center">Room</CardTitle>
+            <CardContent className="flex gap-4 items-center relative">
+              <div className="w-30 h-48 bg-black/10 rounded border border-gray-900"></div>
+              <div className="w-30 h-48 bg-black/10 rounded border border-gray-900"></div>
+              <div className="w-30 h-48 bg-black/10 rounded border border-gray-900"></div>
+              <div className="w-30 h-48 bg-black/10 rounded border border-gray-900"></div>
+            </CardContent>
+            <CardAction className="mx-auto">
+              <Button className="p-4 hover:cursor-pointer hover:scale-102">Avoid Room</Button>
+            </CardAction>
+          </Card>
         </div>
-        <div className="border">
-          <h1>action</h1>
+        <div className="flex flex-col justify-center items-center">
+          <Card>
+            <CardTitle className="text-center">Throwaway</CardTitle>
+            <CardContent className="flex gap-4 items-center relative">
+              <div className="w-30 h-48 bg-black/10 rounded border border-gray-900"></div>
+            </CardContent>
+          </Card>
         </div>
+        <Health/>
+        <div className="flex flex-col justify-center items-center col-span-3">
+          <Card>
+            <CardContent className="flex gap-4 items-center relative">
+              <div className="w-30 h-48 bg-black/10 rounded border border-gray-900"></div>
+            </CardContent>
+            <CardTitle className="text-center">Equipped</CardTitle>
+          </Card>
+        </div>
+        <div></div>
       </div>
     </div>
   )
---
+++ components/Health.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { Card, CardContent, CardTitle } from './ui/card'
+import { Heart } from 'lucide-react'
+
+const Health = () => {
+  const [health, setHealth] = useState<number>(20)
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <Card>
+        <CardTitle className="text-center">Health</CardTitle>
+        <CardContent className="flex gap-4 items-center">
+          <Heart size={24} strokeWidth={4} color={"red"}/>
+          <span className="text-2xl">{health}</span>
+        </CardContent>
+      </Card>
+      </div>
+  )
+}
+
+export default Health
